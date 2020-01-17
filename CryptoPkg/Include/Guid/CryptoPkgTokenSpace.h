9af76e8681408e9884f0c92690b8a209716667c7
Sukerkar, Amol N <amol.n.sukerkar@intel.com>
2020-01-17 14:25:07 -0700

    CryptoPkg: Add CryptoPkg Token Space GUID
    Added CryptoPkg Token Space GUID to be able to define PCDs.
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Signed-off-by: Sukerkar, Amol N <amol.n.sukerkar@intel.com>

---
+++ CryptoPkg/Include/Guid/CryptoPkgTokenSpace.h
@@ -0,0 +1,19 @@
+/** @file
+  GUID for CryptoPkg PCD Token Space.
+
+Copyright (c) 2009 - 2020, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _CRYPTOPKG_TOKEN_SPACE_GUID_H_
+#define _CRYPTOPKG_TOKEN_SPACE_GUID_H_
+
+#define CRYPTOPKG_TOKEN_SPACE_GUID \
+  { \
+    0x7c4b0548, 0xd267, 0x451f, { 0xb5, 0x68, 0x58, 0x4f, 0x82, 0xb2, 0x1c, 0x89 } \
+  }
+
+extern EFI_GUID gEfiCryptoPkgTokenSpaceGuid;
+
+#endif
